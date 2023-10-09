0978c797bede4c9191df3b922bb4674287a8406a
Gregory Dutton <greg.dutton@gmail.com>
2023-10-08 21:27:28 -0500

    lab complete

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,17 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    nodes_to_check = []
+    nodes_to_check.append(self.root)
+    current_node = []
+    while len(nodes_to_check) > 0:
+      current_node = nodes_to_check[0]
+      if current_node['id'] == id:
+        print('ID matches')
+        return current_node
+      elif current_node['children']:
+        for i in range(len(current_node['children'])):
+          nodes_to_check.append(current_node['children'][i])
+      nodes_to_check.pop(0)
+    print('No ID matches')
+    return None
